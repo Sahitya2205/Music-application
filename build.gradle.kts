3fc1718c735966ffaa894975d0d51fba03173123
Sahitya2205 <145531011+Sahitya2205@users.noreply.github.com>
2024-11-19 23:35:26 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+allprojects {
+    repositories {
+        google() // Ensure Google Maven repository is included
+        mavenCentral() // Include Maven Central repository
+    }
+}
